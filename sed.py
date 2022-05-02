dc59bfee171c126b9049399c670d9a16640d2ca9
Matt Compton <matt@tar.black>
2022-05-02 04:17:27 +0000

    grrr

--- sed.py
+++ sed.py
@@ -1,7 +1,9 @@
-import os
+import os,subprocess
 
 text = open("new.service").read()
 text = text.replace("PATH", os.getcwd())
+text = text.replace("GCPATH", subprocess.check_output(["/usr/bin/bash","-c","which gunicorn"]).decode('utf-8').strip())
+
 os.remove("new.service")
 with open("new.service", "w") as f:
     f.write(text)
